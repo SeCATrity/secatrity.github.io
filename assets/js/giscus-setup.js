7fcdfdca30427de10266870190a85cf9cb94ce89
SeCATrity <98455174+SeCATrity@users.noreply.github.com>
2026-03-14 12:47:43 +0000

    Deploying to gh-pages from @ SeCATrity/secatrity.github.io@5f011a2543f832a813ef43e68afe1d9afeb9adff 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SeCATrity/nanjiang.github.io",
+    "data-repo": "SeCATrity/secatrity.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
